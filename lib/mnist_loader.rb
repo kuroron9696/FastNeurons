aefaf073de568036f3024c479c1b4773f61a4c22
karasier <40154200+karasier@users.noreply.github.com>
2020-06-20 17:54:06 +0900

    Revert "v1.2.0"
    This reverts commit f97147b5eb31910e15deb342d6292604673ac986.

--- lib/mnist_loader.rb
+++ lib/mnist_loader.rb
@@ -2,7 +2,6 @@ require_relative 'idx_loader'
 require 'zlib'
 
 # Loader of MNIST.
-# @version 1.1.0
 # @since 1.0.0
 class MNISTLoader
   # constructor
@@ -48,7 +47,7 @@ class MNISTLoader
   # Binarize pixel values to a continuous values from 0 ~ 255 to 0,1.
   # @param [Array] inputs array of pixel values.
   # @return [Array] array of binarized pixel values
-  # @since 1.1.0
+  # @since 1.2.0
   def binarize(inputs)
     return inputs.map{|pixel|
       pixel > 0.5 ? 1.0 : 0.0
